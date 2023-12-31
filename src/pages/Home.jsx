ea2b7d5be3b21876cc16ca334774880f07acd465
prasadkadam2303@gmail.com <prasadkadam2303@gmail.com>
2023-12-31 12:29:49 +0530

    changes by prasad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Home = () => {
   let fetchFunction = async () => {
-    let resp = await fetch("http://localhost:3000/courses")
+    let resp = await fetch("http://localhost:3000/departments")
     let data = await resp.json()
-    console.log(data.departments);
+    console.log(data);
+    data.map((val, index) => {
+      let allFaculty = val.faculty.map((facultyName, ind) => {
+          console.log(facultyName);
+      })
+    })
   }
   fetchFunction()
   return (
